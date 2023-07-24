3f3de7a0445de985bcefb05ec80dfde1f4d4ec70
Gal Nakash <gal@recolabs.ai>
2023-07-24 11:56:10 +0300

    fix lin tests

--- calc_test.py
+++ calc_test.py
@@ -1,12 +1,12 @@
 import unittest
+
+
 class TestCalc(unittest.TestCase):
     def test_add(self):
         self.assertEqual(4, 2 + 2)
 
-
     def test_sub(self):
         self.assertEqual(2, 4 - 2)
-
 
     def test_mul(self):
         self.assertEqual(6, 3 * 2)
