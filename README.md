4c59a52188f2a24269266f65b1d1fc2b6b3a07a2
Shani Arnon <shania@reco.ai>
2023-07-29 11:16:52 +0300

    calc: skeleton for calculator program
      * Add bare bones main calculator program
      * Add framework for calculator unit tests
      * Add readme file with usage instructions
    
    <Signed-off-by: Shani Arnon <shania@recolabs.ai>

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Gal-git-history-ex
+# Calc
+
+## Description
+This is a calculator program. 
+The program accepts a string of numbers and operators and returns the result of the calculation.
+
+
+## Usage
+To use the program, run the following command in the terminal:
+```
+python3 calc.py <operation> <number1> <number2>
+```
+
+
+## Supported Operations
+At the moment no operations are supported.
+
+## Example
+TBD
+
+## Author
+- [**Gal Nakash**]()
---
+++ calc.py
@@ -0,0 +1,27 @@
+import sys
+
+functions = {}
+
+def main():
+    if len(sys.argv) != 4:
+        print('Usage: calculator.py <function> <number> <number>')
+        sys.exit(1)
+
+    function = sys.argv[1]
+    if function not in functions:
+        print('Error: Invalid function. Functions are: {}'.format(', '.join(functions.keys())))
+        sys.exit(1)
+
+    try:
+        a = float(sys.argv[2])
+        b = float(sys.argv[3])
+    except ValueError:
+        print('Error: Invalid numbers. Numbers must be integers.')
+        sys.exit(1)
+
+    result = functions[function](a, b)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
